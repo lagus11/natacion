4d94521b014bb6adee3609c1715dfb1d2c610d8b
Agustin Hernandez Montero <agdehernandezmo@ittepic.edu.mx>
2023-08-08 21:47:30 -0700

    first commit

---
+++ src/natacion/Natacion.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package natacion;
+
+import Model.config.Conexion;
+
+/**
+ *
+ * @author lagus11
+ */
+public class Natacion extends Conexion{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Conexion con = new Conexion();
+        con.getConexion();
+    }
+    
+}
